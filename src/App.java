63d49e1dc786cf8b92486c6b37d803bec23f56ad
Joir Neto <joirneto@bb.com.br>
2023-08-17 13:53:19 -0300

    Criando projeto base

---
+++ src/App.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // // Exibindo informações na Tela
+        // System.out.println("Com quebra de linha");
+        // System.out.print("Sem quebra de linha");
+        // System.out.print("Quebra de linha com caracter de escape\n");
+        // System.out.printf("Formatando informaççoes - %s - %d%n", "String", 2021);
+        // //--------------------------------------------------------------------------
+
+        // // Condicionais
+        // int nota1 = 10;
+        // int nota2 = 5;
+        
+        // if(nota1 < nota2){
+        //     System.out.println("TESTE IF");
+        // } else if (nota1 == 10) {
+        //     System.out.println("TESTE ELSE IF");
+        // } else {
+        //     System.out.println("TESTE ELSE");
+        // }
+
+        // String res = (nota1 > nota2 ? "Aprovado\n" : "Reprovado\n");
+        // System.out.printf("Aluno %s", res);
+
+        // int posicao = 5;
+        // switch(posicao) {
+        //     case 1:
+        //         System.out.printf("Caso %d%n", posicao);
+        //         break;
+        //     case 2:
+        //         System.out.printf("Caso %d%n", posicao);
+        //         break;
+        //     case 3:
+        //         System.out.printf("Caso %d%n", posicao);
+        //         break;
+        //     case 4: case 5: case 6:
+        //         System.out.printf("Caso %d%n", posicao);
+        //         break;
+        //     default:
+        //         System.out.printf("Caso não encontrado\n", posicao);
+        // }
+        // //--------------------------------------------------------------------------
+
+        // Recebendo entrada
+        // Scanner entrada = new Scanner(System.in);
+        // int n1;
+        // int n2;
+        // int res;
+
+        // System.out.print("Digite num1: ");
+        // n1 = entrada.nextInt();
+        // System.out.print("Digite num2: ");
+        // n2 = entrada.nextInt();
+        // res = n1 + n2;
+        // System.out.printf("Soma de %d + %d = %d", n1, n2, res);
+
+        // System.out.print("Digite seu nome: ");
+        // String nome = entrada.nextLine();
+        // System.out.print("Digite seu sobrenome: ");
+        // String sobrenome = entrada.nextLine();
+        // System.out.print("Ola " + nome + " " + sobrenome);
+        
+    }
+}
